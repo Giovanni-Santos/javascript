fc1b32191738570a7df1c7f105ff5de428db7bf3
Giovanni Alex dos Santos <giovannialex9@hotmail.com>
2024-02-19 14:50:50 -0300

    .

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 61
+console.log(`A velocidade do seu carro e: ${vel}Km/h`);
+if(vel > 60) //Condicao Simples
+{
+    console.log('Voce ultrapasso o limite de velocidade. MULTADO!!!');
+}
+console.log('Dirija sempre usando cinto de seguranca');
