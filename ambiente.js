8563df1fba7e3826f8c445aa0694249f3454b329
Giovanni Alex dos Santos <giovannialex9@hotmail.com>
2024-02-26 20:55:20 -0300

    .

---
+++ ambiente.js
@@ -0,0 +1,15 @@
+/*
+    console.log('Oi');
+    console.log('Oi');
+    console.log('Oi');
+    console.log('Oi');
+    console.log('Oi');
+*/
+
+//Usando repeticao
+const contador = 1
+
+while(contador <= 5){
+    console.log('Oi');
+    contador++
+}
