fd38bba0d85c36b334c6aa676c4629400ec90089
Giovanni Alex dos Santos <giovannialex9@hotmail.com>
2024-02-28 02:35:12 -0300

    .

--- ex-teste/script.js
+++ ex-teste/script.js
@@ -8,10 +8,8 @@ apagar.addEventListener('click', apagou)
 function apareceu(){
     fundo.style.backgroundImage = 'url(ben10.jpg)'
     fundo.style.display = 'block'
-    console.log('Oi Cauazinho');
 }
 
 function apagou(){
     fundo.style.backgroundImage = display = 'none'
-    console.log('Tchau Cauazinho');
 }
