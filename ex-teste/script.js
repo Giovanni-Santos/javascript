6fd624523c9e887856533ae4e879d3744dfb3293
Giovanni Alex dos Santos <giovannialex9@hotmail.com>
2024-02-27 23:57:08 -0300

    .

---
+++ ex-teste/script.js
@@ -0,0 +1,14 @@
+const aparecer = document.getElementById('aparecer')
+const apagar = document.getElementById('apagar')
+const fundo = document.getElementById('fundo')
+
+aparecer.addEventListener('click', apareceu)
+apagar.addEventListener('click', apagou)
+
+function apareceu(){
+    
+}
+
+function apagou(){
+    
+}
--- teste.js
+++ teste.js
@@ -1,16 +1,24 @@
 let c = 1
 
+for(c=1;c<=5;c++){
+    console.log(c);
+}
+
 //Enquando condicao = true => execute
+/*
 while(c <= 6){
     console.log('Contador ' + c);
     c++
 }
+*/
 
 //Execute enquanto => condicao = true  
+/*
 do{
     console.log('Contador ' + c);
     c++
 }while(c <= 6)
+*/
 
 /*
 console.log('Oi');
